1e761a83900c3a8b105c6484dd5a4117e17b6439
Dennis Leyva <dennisleyva8@gmail.com>
2021-11-16 14:43:40 -0700

    working on btn

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -14,4 +14,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+document.getElmenentById("generate").addEventListener("click", writePassword);
+    var window.alert("would you like a new password"); 
